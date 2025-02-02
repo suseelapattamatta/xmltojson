70cc31ed0debcfa4e164b539b00dba423d689c9a
Suseela Pattamatta <souseilaw@yahoo.co.uk>
2025-02-02 14:39:07 -0500

    Update xmltojson.py

--- xmltojson.py
+++ xmltojson.py
@@ -8,7 +8,7 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
   xml_file = uploaded_file.read()
-  json_file = json_loads(json.dumps(xmltodict.parse(xml_file)))
+  json_file = json.loads(json.dumps(xmltodict.parse(xml_file)))
   st.write(json_file)
 else:
   st.text("No XML file uploaded")
