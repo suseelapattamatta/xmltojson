7f8cdfd04ab6059f32cc398a79900c07c4d3bb74
Suseela Pattamatta <souseilaw@yahoo.co.uk>
2025-02-02 14:21:06 -0500

    Update xmltojson.py

--- xmltojson.py
+++ xmltojson.py
@@ -1,1 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+st.title("XML to JSON Converter")
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  xml_file = uploaded_file.read()
+  json_file = json_loads(json.dumps(xmltodict.parse(xml_file)))
+  st.write(json_file)
+    
